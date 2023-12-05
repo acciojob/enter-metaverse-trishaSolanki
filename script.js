cae2fed616b5d85a408b3ad4000cac2ff571e5d6
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-05 17:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded',function(){
-const btn = document.getElementById('status');
-const enter = document.getElementById('enterBtn');
+document.addEventListener("DOMContentLoaded", function() {
+  const statusParagraph = document.getElementById('status');
+  const enterButton = document.getElementById('enterBtn');
 
-enter.addEventListener('click',function() {
-	btn.innerText = '<h1>Enter the Metaversion</h1>'';
-});
+  enterButton.addEventListener('click', function() {
+    statusParagraph.innerHTML = '<h1>Entered Metaverse</h1>';
+  });
 });
