1ef16cdaba76e37777daa601c77a765e8374ed70
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-05 17:38:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',function(){
+const btn = document.getElementById('status');
+const enter = document.getElementById('enterBtn');
+
+enter.addEventListener('click',function() {
+	btn.innerText = '<h1>Enter the Metaversion</h1>'';
+});
+});
